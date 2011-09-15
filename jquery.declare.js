d301e00a69e6a50106168e45486ec4f12fb88688
acatl pacheco <acatl.pacheco@gmail.com>
2011-09-15 12:22:58 -0500

    first commit

---
+++ jquery.declare.js
@@ -0,0 +1,38 @@
+/*
+* 'declare' plugin for jQuery
+*
+* Copyright (c) 2011 Acatl Pacheco https://github.com/acatl/jquery.declare
+* Licensed under the MIT License.
+* http://jquery.org/license
+*/
+(function( $ ){
+	$.extend({
+		declare: function (namespace, object) {
+			var scopes = namespace.split(".");
+			var scopeLength = scopes.length;
+			var scopeObject = window;
+			if(scopeLength==0) return false;
+			for(var i=0; i < scopeLength; i++) {
+				var scope = scopes[i];
+				if(i!=scopeLength-1) {
+					if(!scopeObject.hasOwnProperty(scope)) {
+						scopeObject[scope] = {};
+					}
+					scopeObject = scopeObject[scope];
+				} else {
+					if(!scopeObject.hasOwnProperty(scope)) {
+						scopeObject[scope] = {};
+					} 
+					switch(typeof object) {
+						case "object":
+							$.extend(scopeObject[scope], object);
+						break;
+						default: 
+							scopeObject[scope] = object;
+					}
+					
+				}
+			}
+		}
+	});
+})( jQuery );
